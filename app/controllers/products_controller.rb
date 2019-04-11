7ee107d93210c2c4231eb75b3c1dafbd7bcfbbbe
Oblum1989 <oscar.blum@gmail.com>
2019-04-10 20:07:26 -0500

    funcionalidad crear y listar producto

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+    @categories = Category.all
+  end
+
+  def create  
+    @product = Product.new(product_params)
+    
+    if @product.save 
+      redirect_to products_path, notice: "El producto ha sido guardado exitosamente"
+    else
+      flash[:alert] = "El producto no ha sido guardado, por favor repetir la operacion"
+      render :new
+    end
+  end
+  private
+  
+    def product_params
+      params.require(:product).permit(:name, :price, category_ids:[])
+    end
+end
